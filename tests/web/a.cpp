2726399f76aca691c31e026136105ef96bfafe13
Todd Fleming <tbfleming@gmail.com>
2021-02-05 11:43:36 -0500

    tasks may return values to awaiting tasks

--- tests/web/a.cpp
+++ tests/web/a.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstring>
 #include <experimental/coroutine>
+#include <string>
 
 namespace clarion
 {
@@ -35,51 +36,69 @@ namespace clarion
         void await_resume() {}
     };
 
-    // A task is a coroutine which supports async operations. Once created, a task may be:
-    //  * started exactly once, or
-    //  * co_awaited on by another task exactly once, or
-    //  * be destroyed.
-    //
-    // Starting a task or co_awaiting on a task detaches ownership from the task
-    // object; the running task is automatically destroyed when it's done executing.
-    struct task
-    {
-        struct promise_type
-        {
-            coro::coroutine_handle<void> continuation;
+    template <typename Ret = void>
+    struct task;
 
-            promise_type() { printf("promise_type::promise_type\n"); }
-            ~promise_type() { printf("promise_type::~promise_type\n"); }
-            auto initial_suspend() { return coro::suspend_always(); }
-            auto final_suspend() noexcept
-            {
-                if (continuation)
-                    continuation.resume();
-                return coro::suspend_never();
-            }
-            task get_return_object() { return {this}; }
-            void unhandled_exception() { std::abort(); }
-            void return_void() {}
-        };
+    template <typename Ret, typename Derived>
+    struct task_promise_base
+    {
+        coro::coroutine_handle<void> continuation;
+
+        task_promise_base() { printf("task_promise::task_promise\n"); }
+        ~task_promise_base() { printf("task_promise::~task_promise\n"); }
+        auto initial_suspend() { return coro::suspend_always(); }
+        auto final_suspend() noexcept
+        {
+            if (continuation)
+                continuation.resume();
+            return coro::suspend_never();
+        }
+        task<Ret> get_return_object() { return {(Derived *)this}; }
+        void unhandled_exception() { std::abort(); }
+    };
+
+    template <typename Ret>
+    struct task_promise : task_promise_base<Ret, task_promise<Ret>>
+    {
+        Ret *retval = nullptr;
+
+        template <typename T>
+        void return_value(T &&value)
+        {
+            if (retval)
+                *retval = std::forward<T>(value);
+        }
+    };
+
+    template <>
+    struct task_promise<void> : task_promise_base<void, task_promise<void>>
+    {
+        void return_void() {}
+    };
+
+    template <typename Ret>
+    struct task_base
+    {
+        using promise_type = task_promise<Ret>;
 
         promise_type *promise = nullptr;
 
-        task() = default;
-        task(promise_type *promise) : promise{promise} {}
-        task(const task &) = delete;
-        task(task &&src)
+        task_base() = default;
+        task_base(promise_type *promise) : promise{promise} {}
+        task_base(const task_base &) = delete;
+        task_base(task_base &&src)
         {
             *this = std::move(src);
         }
 
-        ~task()
+        ~task_base()
         {
             if (promise)
                 coro::coroutine_handle<promise_type>::from_promise(*promise).destroy();
         }
 
-        task &operator=(const task &) = delete;
-        task &operator=(task &&src)
+        task_base &operator=(const task_base &) = delete;
+        task_base &operator=(task_base &&src)
         {
             promise = src.promise;
             src.promise = nullptr;
@@ -95,7 +114,7 @@ namespace clarion
         bool await_ready() { return false; }
 
         // TODO: should return a coro::coroutine_handle<void> to enable efficient task switching,
-        // but that causes clang to report an error: "WebAssembly 'tail-call' feature not enabled".
+        // but that causes clang 11.0.0 to report an error: "WebAssembly 'tail-call' feature not enabled".
         // This will also require changes to promise_type::final_suspend().
         void await_suspend(coro::coroutine_handle<void> co)
         {
@@ -103,25 +122,57 @@ namespace clarion
             coro::coroutine_handle<promise_type>::from_promise(*promise).resume();
             promise = nullptr;
         }
+    }; // task_base
+
+    // A task is a coroutine which supports async operations. Once created, a task may be:
+    //  * started exactly once using start(), or
+    //  * co_awaited on by another task exactly once, or
+    //  * be destroyed
+    //
+    // Starting a task or co_awaiting on a task detaches ownership from the task
+    // object; the running task is automatically destroyed when it's done executing.
+    template <typename Ret>
+    struct task : task_base<Ret>
+    {
+        using task_base<Ret>::task_base;
+
+        Ret retval = {};
+
+        void await_suspend(coro::coroutine_handle<void> co)
+        {
+            this->promise->retval = &retval;
+            task_base<Ret>::await_suspend(co);
+        }
+
+        Ret await_resume() { return std::move(retval); }
+    }; // task
+
+    template <>
+    struct task<void> : task_base<void>
+    {
+        using task_base<void>::task_base;
 
         void await_resume() {}
-    }; // task
+    };
 } // namespace clarion
 
-clarion::task testco(const char *s, uint32_t delay_ms)
+clarion::task<std::string> testco(std::string s, uint32_t delay_ms)
 {
+    std::string result;
     for (int i = 0; i < 10; ++i)
     {
-        printf("s = \"%s\", i = %d\n", s, i);
+        printf("s = \"%s\", i = %d\n", s.c_str(), i);
+        result += "(" + s + ")";
         co_await clarion::later{delay_ms};
     }
-    printf("s = %s, finished\n", s);
+    printf("s = \" %s \", finished\n", s.c_str());
+    co_return result;
 }
 
-clarion::task testco2(uint32_t delay_ms)
+clarion::task<> testco2(uint32_t delay_ms)
 {
-    co_await testco("loop 1", delay_ms);
-    co_await testco("loop 2", delay_ms);
+    printf("loop 1 returned: %s\n", (co_await testco("loop 1", delay_ms)).c_str());
+    printf("loop 2 returned: %s\n", (co_await testco("loop 2", delay_ms)).c_str());
     printf("testco2 finished\n");
 }
 
