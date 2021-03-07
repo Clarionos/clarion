d82799309ab7ae000e95b7231dad666aa5170a85
Todd Fleming <tbfleming@gmail.com>
2021-03-07 16:48:28 -0500

    add pwa to build

---
+++ ts/pwa.webpack.config.js
@@ -0,0 +1,42 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+const dist = path.resolve(__dirname, "../pwa");
+
+module.exports = {
+    mode: "development",
+    entry: "${CMAKE_CURRENT_BINARY_DIR}/src/programs/clarionpwa/index.ts",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            configFile:
+                                "${CMAKE_CURRENT_BINARY_DIR}/pwa.tsconfig.json",
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "src/programs/clarionpwa/index.html", to: dist },
+            ],
+        }),
+    ],
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        symlinks: false,
+    },
+    output: {
+        filename: "index.js",
+        path: dist,
+    },
+};
