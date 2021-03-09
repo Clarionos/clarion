5995120cc076e560fa1365763ccf83165eaf08e9
Spark Plug <sparkplug0025@gmail.com>
2021-03-09 01:15:00 -0500

    ts build on cmake

---
+++ packages/tester/src/index.ts
@@ -0,0 +1,24 @@
+import { Context } from "@clarionos/bios";
+import { readFileSync } from "fs";
+
+const runWasm = async (path: string, args: string[]) => {
+  const context = new Context(args);
+  try {
+    const wasm = readFileSync(path);
+    await context.instanciate(wasm);
+    (context.instance!.exports as any)._start();
+  } catch (e) {
+    if (context.consoleBuf) console.log(context.consoleBuf);
+    console.error(e);
+    process.exit(1);
+  }
+};
+
+if (process.argv.length < 3) {
+  console.error(
+    `Usage: ${process.argv[0]} ${process.argv[1]} wasm_path wasm_args...`
+  );
+  process.exit(1);
+} else {
+  runWasm(process.argv[2], process.argv.slice(2));
+}
--- programs/clariond/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const DATABASE = process.env.CLARION_DATABASE || "clariondata";
-export const CLARION_WASM_PATH =
-  process.env.CLARION_WASM_PATH || "./clarion.wasm";
