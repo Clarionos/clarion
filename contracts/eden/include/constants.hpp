80c3ac3ddd321d40d006818e6f2b72c9aba59430
SparkPlug0025 <79721020+sparkplug0025@users.noreply.github.com>
2021-04-01 11:50:02 -0400

    Revert "init eden contract (#44)" (#45)
    This reverts commit c68e3bd0b7d0b03bad4209a1401f2b9a30f8e98e.

--- contracts/eden/include/constants.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <eosio/asset.hpp>
-
-namespace eden
-{
-   inline const eosio::symbol default_token{"EOS", 4};
-   inline const uint64_t default_scope = 0;
-}  // namespace eden
